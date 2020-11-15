929bda7bdf2b1c4ca9db751a2efa8f10625c8985
Dmitry <gabrial93@gmail.com>
2020-11-15 22:11:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tab/SeconfActivity.java
@@ -0,0 +1,20 @@
+package com.example.tab;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SeconfActivity extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.second_tab,
+                container, false );
+    }
+}
